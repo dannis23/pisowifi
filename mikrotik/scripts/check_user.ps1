63ef2135e2006e12bbb819d0bb42e79e0e8bca26
FDC.Dannis <dannisalejandrino@gmail.com>
2024-08-22 16:14:45 +0800

    pisowifi_v8: change script for login

--- mikrotik/scripts/check_user.ps1
+++ mikrotik/scripts/check_user.ps1
@@ -1,28 +1,33 @@
-#Check User
-:log warning "Check All User";
+# Check User
+:log warning "Check All Users";
 :foreach user in=[/ip hotspot user print as-value] do={
     :local userName ($user->"name");
     :local limitUptime [/ip hotspot user get $userName limit-uptime];
     :local uptime [/ip hotspot user get $userName uptime];
     :local interval "00:00:00";  # Default interval to avoid unset variable issues
-    :local schedName [/system schedule find name=$userName]
+    :local profile [/ip hotspot user get $userName profile];
+    :local schedName [/system schedule find name=$userName];
 
-    # Skip default-trial user
-    :if ($userName != "default-trial") do={  
-        #Set schedule for unused user
+
+    # Skip default-trial user and admin profile
+    :if ($userName != "default-trial" && $profile != "admin") do={  
+
+        # Set schedule for unused user
         :if ($uptime = "00:00:00") do={
             :if ([:len $schedName] = 0) do={
-                :set interval "23:59:59"
-                :log warning "Scheduled removal for user: $userName with interval $interval"
+                :set interval "23:59:59";
+                :log warning "Scheduled removal for user: $userName with interval $interval";
                 /system schedule add name=$userName interval=$interval on-event="/ip hotspot active remove [find user=$userName]\r\n/ip hotspot user remove [find name=$userName]\r\n/system schedule remove [find name=$userName]";
             }
         } 
 
-        #Delete time out user
+        # Delete timed-out user
         :if ($limitUptime <= $uptime) do={
-            :log warning "delete time out name: $userName"
-            /ip hotspot user remove [find name=$userName]
-            /system schedule remove [find name=$userName]
+            :log warning "Deleting timed-out user: $userName";
+            /ip hotspot user remove [find name=$userName];
+            /system schedule remove [find name=$userName];
         }
+    } else={
+        :log warning "Skipping user: $userName";
     }
 }
